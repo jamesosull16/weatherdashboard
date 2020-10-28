4e658e2a8908a29f2dc3a07316bc9b7cb6d167b0
jamesosull16 <jamesosull16@gmail.com>
2020-10-28 14:04:30 -0600

    start of js and more to the html

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-var weatherRequestUrl =
-  "api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key}";
+var cityNameInput = document.querySelector("#inputCityName");
+var searchButton = document.querySelector("#searchBtn");
+
+//empty array for use to store search history
+var searchHistory = [];
+
+function getWeather(inputCityName) {
+  var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${inputCityName}&units=imperial&appid=789c208b0036afe3defb6d2d5ebbbe29`;
+
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      searchHistory.push(inputCityName);
+      console.log(searchHistory);
+      //call the new function to display search history
+      $("#inputCityName").val("");
+    });
+}
+
+// create a new function that manipuates the dom to add the most recent search to a search history div setSearch()
+function setSearch() {
+  var historyDiv = $("#search-history");
+  for (var i = 0; searchHistory.length; i++) {
+    var newSearchHistory = $("<button>" + searchHistory[i] + "</button>");
+    historyDiv.prepend(newSearchHistory);
+  }
+}
+
+$("#searchBtn").on("click", function () {
+  getWeather(cityNameInput.value.trim());
+});
